9835c8d94058f4d4a6c76de7fd3afae17cf5e8c0
afuet <73286581+lllk140@users.noreply.github.com>
2022-11-29 00:56:11 +0800

    移除原登陆方法，包括扫码

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from application.apps.app import App
 
+from application.message import showinfo
+
 if __name__ == '__main__':
     app = App()
+    showinfo("提示", "原登陆方法已弃用，使用新登陆器[导入登陆]已继续")
     app.mainloop()
