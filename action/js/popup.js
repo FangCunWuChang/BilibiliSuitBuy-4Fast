db4341acdac0533610230ee4af9a311c6d501de7
afuet <73286581+lllk140@users.noreply.github.com>
2023-02-10 22:09:24 +0800

    Delete action directory

--- action/js/popup.js
+++
@@ -1,6 +0,0 @@
-(async function() {
-    const config = await getLocalContent("config") || {"style": {}};
-    const style = config["style"];
-    
-    document.body.style.backgroundColor = style["popup-background-color"];
-})();
